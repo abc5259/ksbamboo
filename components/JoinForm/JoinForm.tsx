54a73704138ade6c3f63fedaed7e4acc6ea1a336
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-15 01:09:35 +0900

    feat: JoinForm clear

--- components/JoinForm/JoinForm.tsx
+++ components/JoinForm/JoinForm.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { ErrorMessage } from "../LoginForm/LoginFormStyles";
-import { Form, JoinContainer, Title, Input, Button } from "./JoinFormStyles";
+import {
+  Form,
+  JoinContainer,
+  Title,
+  Input,
+  Button,
+  Select,
+  SelectWrapper,
+  InputWrapper,
+  Label,
+} from "./JoinFormStyles";
+
+const enterYear: number[] = [
+  2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010,
+  2009, 2008,
+];
 
 interface IJoinForm {
+  enterYear: string;
   email: string;
   nickname: string;
   password: string;
@@ -13,26 +29,84 @@ interface IJoinForm {
 const JoinForm = () => {
   const {
     register,
+    watch,
     handleSubmit,
     formState: { errors },
-  } = useForm<IJoinForm>();
+  } = useForm<IJoinForm>({
+    defaultValues: {
+      email: "@ks.ac.kr",
+    },
+  });
+
+  const onVaild = (data: IJoinForm) => {
+    console.log(data);
+  };
+
   return (
     <JoinContainer>
       <Title>KSB 회원가입</Title>
-      <Form>
-        <Input
-          {...register("email", {
-            required: "이메일을 적어주세요",
-            validate: value =>
-              value.includes("@") ? true : "이메일이어야 합니다.",
-          })}
-          name="email"
-          placeholder="이메일"
-        />
-        <ErrorMessage></ErrorMessage>
-        <Input name="nickname" placeholder="닉네임" />
-        <Input name="password" placeholder="비밀번호" />
-        <Input name="passwordConfoirm" placeholder="비밀번호 확인" />
+      <Form onSubmit={handleSubmit(onVaild)}>
+        <SelectWrapper>
+          <Label>입학년도</Label>
+          <Select
+            {...register("enterYear", {
+              validate: value => {
+                if (value.trim() == "연도 선택 (학번)".trim()) {
+                  return "학번을 선택하세요";
+                } else {
+                  return true;
+                }
+              },
+            })}
+            id="enter_year"
+          >
+            <option>연도 선택 (학번)</option>
+            {enterYear.map(year => (
+              <option key={year} value={year}>
+                {year}학번
+              </option>
+            ))}
+          </Select>
+          <ErrorMessage>{errors.enterYear?.message}</ErrorMessage>
+        </SelectWrapper>
+        <InputWrapper>
+          <Label>경성대학교 이메일</Label>
+          <Input
+            {...register("email", {
+              required: "학교 이메일을 적어주세요",
+              validate: value =>
+                value.includes("@ks.ac.kr")
+                  ? true
+                  : "학교 이메일이어야 합니다.",
+            })}
+            name="email"
+            placeholder="이메일"
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+        </InputWrapper>
+        <InputWrapper>
+          <Label>비밀번호</Label>
+          <Input
+            {...register("password", {
+              required: "비밀번호를 입력하세요",
+            })}
+            type="password"
+            name="password"
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
+        </InputWrapper>
+        <InputWrapper>
+          <Label>비밀번호 확인</Label>
+          <Input
+            {...register("passwordConfirm", {
+              required: "비밀번호 확인을 입력하세요",
+              validate: value =>
+                value !== watch("password") ? "비밀번호가 같지 않습니다" : true,
+            })}
+            name="passwordConfirm"
+          />
+          <ErrorMessage>{errors.passwordConfirm?.message}</ErrorMessage>
+        </InputWrapper>
         <Button type="submit">회원가입</Button>
       </Form>
     </JoinContainer>
--- components/JoinForm/JoinFormStyles.tsx
+++ components/JoinForm/JoinFormStyles.tsx
@@ -12,11 +12,38 @@ export const Title = styled.h1`
   font-size: 22px;
   margin-bottom: 8px;
 `;
+
 export const Form = styled.form`
   width: 80%;
   display: flex;
   flex-direction: column;
 `;
+
+export const SelectWrapper = styled.div`
+  select {
+    width: 100%;
+    &:focus {
+      border-color: inherit;
+    }
+  }
+`;
+
+export const InputWrapper = styled.div``;
+
+export const Select = styled.select`
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #f9f9f9;
+  border-radius: 12px;
+`;
+
+export const Label = styled.label`
+  display: block;
+  padding: 5px;
+  font-size: 12px;
+  color: #8b949e;
+`;
+
 export const Input = styled.input.attrs({ require: true })`
   width: 100%;
   font-size: 18px;
