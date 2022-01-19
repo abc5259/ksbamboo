444929d45cda4b724f2aaaaf879f006ae61379e8
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-19 17:41:14 +0900

    feat: Board CSS working

--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,6 +1,15 @@
 import { useRecoilValue } from "recoil";
 import { boardsAtom } from "../../atom/atoms";
-import { BoardStyle } from "./BoardStyles";
+import {
+  Bottom,
+  BoardStyle,
+  Content,
+  Main,
+  Title,
+  Author,
+  Like,
+  LikeWrapper,
+} from "./BoardStyles";
 
 interface IBoardProps {
   boardId: number;
@@ -11,10 +20,27 @@ const Board = ({ boardId }: IBoardProps) => {
   const board = boards.find(board => board.id === boardId);
   return (
     <BoardStyle>
-      <h1>{board?.title}</h1>
-      <p>{board?.content}</p>
-      <h3>{board?.user.nickname}</h3>
-      <h3>{board?.department}</h3>
+      <Main>
+        <Title>{board?.title}</Title>
+        <Content>{board?.content}</Content>
+      </Main>
+      <Bottom>
+        <Author>
+          By{" "}
+          <span>
+            {board?.status === "익명" ? "익명" : board?.user.nickname}
+          </span>
+        </Author>
+        <LikeWrapper>
+          <Like viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M18 1l-6 4-6-4-6 5v7l12 10 12-10v-7z"
+            ></path>
+          </Like>
+          <span>100</span>
+        </LikeWrapper>
+      </Bottom>
     </BoardStyle>
   );
 };
--- components/Board/BoardStyles.tsx
+++ components/Board/BoardStyles.tsx
@@ -1,7 +1,50 @@
 import styled from "styled-components";
 
 export const BoardStyle = styled.div`
-  height: 300px;
-  border-radius: 10px;
+  max-width: 450px;
+  height: 350px;
+  border-radius: 5px;
   background-color: ${props => props.theme.bgColor.lighter};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const Main = styled.div`
+  padding: 20px 10px;
+`;
+export const Title = styled.h3`
+  font-size: 1rem;
+  font-weight: 800;
+  margin-bottom: 10px;
+`;
+
+export const Content = styled.p`
+  font-size: 0.9rem;
+`;
+
+export const Bottom = styled.div`
+  height: 10%;
+  padding: 20px 10px;
+  border-top: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 0.7rem;
+`;
+
+export const Author = styled.div`
+  span {
+    font-weight: 800;
+  }
+`;
+
+export const LikeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Like = styled.svg`
+  width: 0.8rem;
+  height: 0.8rem;
+  margin-right: 5px;
 `;
