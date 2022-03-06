adc5b9781fc82d41684f69689f18fe2881d8f913
LeeJaeHoon <dlwogns3413@naver.com>
2022-03-06 22:49:25 +0900

    feat:sse example code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,29 @@ import { allBoardsAPI, getFavoriteBoardsAPI } from "../apis/board";
 import IBoard from "../interfaces/board";
 import { getUserAPI } from "../apis/user";
 import User from "../interfaces/user";
-import { AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import Home from "../components/templates/Home/Home";
 import type { NextPage } from "next";
+import { BASE_URL } from "../utils/baseUrl";
+import { useEffect } from "react";
 
 const HomePage: NextPage = () => {
   const { data: boards } = useQuery<IBoard[]>("allboards", allBoardsAPI);
+  useEffect(() => {
+    const evtSource = new EventSource(`${BASE_URL}/boards/events`, {
+      withCredentials: true,
+    });
+    console.log(evtSource);
+    evtSource.onmessage = ({ data }) => {
+      console.log("New message", JSON.parse(data));
+    };
+    axios.post(`${BASE_URL}/boards/emit`).then(response => {
+      evtSource.onmessage = ({ data }) => {
+        console.log("New message", JSON.parse(data));
+      };
+    });
+  }, []);
+
   return (
     <>
       <Home boards={boards} />
