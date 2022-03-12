28345b68e760dce5cea773949b75bd66371dab2f
LeeJaeHoon <dlwogns3413@naver.com>
2022-03-13 04:41:23 +0900

    fix: NotificationPage width변경

--- components/layouts/HeaderLayout/HeaderLayoutStyles.tsx
+++ components/layouts/HeaderLayout/HeaderLayoutStyles.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const StyledHeaderLayout = styled.div`
   margin: 0 auto;
-  width: 70vw;
+  width: 90vw;
   padding: 80px 0;
 `;
--- components/organisms/NotificationCards/NotificationCardsStyles.tsx
+++ components/organisms/NotificationCards/NotificationCardsStyles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledNotificationCards = styled.div`
-  width: 450px;
+  max-width: 450px;
   margin: 0 auto;
   background-color: #fff;
   border-radius: 7px;
